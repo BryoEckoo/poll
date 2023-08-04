5a35172fa38a9b409288211368f5df9c734b8e6b
Brian Rono <chikoe26@gmail.com>
2023-08-04 17:18:54 +0300

    db config 7

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Create a virtual environment and activate it
-python -m venv venv
-source venv/bin/activate
+Python3.11.4 python -m venv venv
+Python3.11.4 source venv/bin/activate
 
 # Install the requirements
 Python3.11.4 pip install -r requirements.txt
 python3.11.4  manage.py collectstatic --noinput --clear
 
 # Run database migrations
-python manage.py migrate
+Python3.11.4 python manage.py migrate
 
 # Start the Django development server (you may need to adjust the host and port as per Vercel's requirements)
 
