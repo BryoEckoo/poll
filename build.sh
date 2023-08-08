9a47f58657d1b5cedf37510428894beaca6c8b80
Brian Rono <chikoe26@gmail.com>
2023-08-08 09:17:32 +0300

    11

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-
-Python3.11.4 -m pip install -r requirements.txt
-python3.11.4  python manage.py collectstatic --noinput --clear
+Python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
