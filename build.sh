3c271a3b40ed7a32786ead54e29c08e99a9d1618
Brian Rono <chikoe26@gmail.com>
2023-08-08 11:03:29 +0300

    18

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-  python3.9 python -m venv venv
- python3.9 venv/Scripts/activate
+ python3.9 polll\Scripts\activate.bat
  python3.9 -m pip install -r requirements.txt
  python3.9 python manage.py migrate
  python3.9 manage.py collectstatic --noinput --clear
