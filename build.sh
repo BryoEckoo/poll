bc0d9d048c655425c606b6087a29cde4443f1a37
Brian Rono <chikoe26@gmail.com>
2023-08-08 10:27:29 +0300

    17

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
   python3.9 python -m venv venv
- python3.9 source venv/Scripts/activate
+ python3.9 venv/Scripts/activate
  python3.9 -m pip install -r requirements.txt
+ python3.9 python manage.py migrate
  python3.9 manage.py collectstatic --noinput --clear
