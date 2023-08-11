eab37af8b519927b185f72df9eb0c8fc78607754
Brian Rono <chikoe26@gmail.com>
2023-08-11 11:03:14 +0300

    60

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
- python3.9 polll\Scripts\activate.bat
- python3.9 -m pip install -r requirements.txt
- python3.9 python manage.py migrate
- python3.9 manage.py collectstatic --noinput --clear
+#!/bin/bash
+
+# Activate the virtual environment (if needed)
+source polll/Scripts/activate  # Uncomment this line if using a virtual environment
+
+# Install requirements (if not already installed)
+python3.9 -m pip install -r requirements.txt
+
+# Apply database migrations
+python3.9 manage.py migrate
+
+# No need to collectstatic if static files are already collected
+
+# Deactivate the virtual environment (if needed)
+# deactivate  # Uncomment this line if using a virtual environment
