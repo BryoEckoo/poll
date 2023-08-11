e7d716052631f3883590a784626560080b819401
Brian Rono <chikoe26@gmail.com>
2023-08-11 13:12:08 +0300

    73

--- bitpoll/settings.py
+++ bitpoll/settings.py
@@ -34,8 +34,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-
-STATIC_ROOT = os.path.join(ROOT_DIR, '_static')
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -90,6 +89,7 @@ INSTALLED_APPS = [
     'friendlytagloader',
     'encrypted_model_fields',
     'django_token_bucket',
+    'psycopg2-binary',
 ]
 
 MIDDLEWARE = [
