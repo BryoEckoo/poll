e442ca039ace657268092314b2246fa975c5bef1
Brian Rono <chikoe26@gmail.com>
2023-08-11 12:48:15 +0300

    70

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
- python3.9 -m venv virtual
- 
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
+#!/bin/bash
+
+# Create and activate the virtual environment
+python3.9 -m venv polll
+source polll/bin/activate
+
+# Install project dependencies
+pip install -r requirements.txt
+
+# Migrate the database
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput --clear
+
+# Deactivate the virtual environment
+deactivate
