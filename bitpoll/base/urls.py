73fd73a332396959d185fa5d6e2900b17d8f2467
Brian Rono <chikoe26@gmail.com>
2023-08-15 12:32:21 +0300

    99

--- bitpoll/base/urls.py
+++ bitpoll/base/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -12,3 +14,8 @@ urlpatterns = [
     path('autocomplete', views.autocomplete, name='base_autocomplete'),
     path('problems', views.problems, name='base_problems'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
+
--- bitpoll/settings.py
+++ bitpoll/settings.py
@@ -36,8 +36,8 @@ MEDIA_URL = '/media/'
 # Example: "/var/www/example.com/static/"
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 # STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-CSS_PATH = os.environ.get('CSS_PATH', '/static/css')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
