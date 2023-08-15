7d844094e49a59c50418fcd44518f5b2dfec526f
Brian Rono <chikoe26@gmail.com>
2023-08-15 12:40:08 +0300

    100

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
  python3.9 polll\Scripts\activate.bat
- pip install whitenoise
- pip freeze > requirements.txt
- pip install -r requirements.txt
+ python3.9 -m pip install django
+ python3.9 -m pip install whitenoise
+ python3.9 -m pip freeze > requirements.txt
+ python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
  python3.9 python manage.py migrate
