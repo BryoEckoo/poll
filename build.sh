e9c52445c4a6c02c719f9caffcd6fc601f474504
Brian Rono <chikoe26@gmail.com>
2023-08-15 09:30:12 +0300

    93

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
  python3.9 python manage.py migrate
  python3.9 -m pip install django
  python3.9 -m pip install django_simple_csp
+ python3.9 -m pip install markdownify
 
  python3.9 -m pip install psycopg2-binary
  python3.9 -m pip install whitenoise
  python3.9 -m pip freeze > requirements.txt
-python3.9 -m pip install -r requirements.txt
+ python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
